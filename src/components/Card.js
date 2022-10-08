23f8c5e8c6ac8195d3a7a6bc2dec3cf7679f98fc
syedrameezahmed18 <noir.ramsam@gmail.com>
2022-10-08 11:58:00 +0500

    part 6

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ const Card = ({id,price, title, description}) => {
   return (
     <div className={`${id === 0 ? 'main-card':'main-card-green'}`}>
         <p className={'o-price'}>{price}</p>
-        <p className="title">{title}</p>
+        <p className={'title'}>{title}</p>
         <p className="desc">{description}</p>
         <p className={'orange-btn'} style={{cursor:"pointer"}}>ORDER NOW</p>
     </div>
